ae1ab77220b750a4d141bc45bf36b5aafac37bc7
p0904294 <skhelifa1@gmail.com>
2017-06-27 15:06:59 +0200

    Récupération des tweets par HTTP

--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -10,16 +10,30 @@
 var tweet1 = {auteur: 'Safa', contenu: 'Salut'}
 var tweet2 = {auteur: 'test2', contenu: 'Salut'}
 var tweet3 = {auteur: 'test3', contenu: 'Salut'}
+import Vue from 'vue'
+import Resource from 'vue-resource'
+Vue.use(Resource)
 import Tweet from './Tweet'
 export default {
 
   name: 'timeline',
   components: {Tweet},
+  methods: {
+    fetchTweets: function () {
+      this.$http.get('http://localhost:8080/list').then(response => {
+        this.tweets = response.body
+      }, response => {
+        console.log('error')
+      })
+    }
+  },
   data () {
     return {
-
       tweets: [tweet1, tweet2, tweet3]
     }
+  },
+  created () {
+    this.fetchTweets()
   }
 }
 </script>
